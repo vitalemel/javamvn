1d349e64abba92e7228cb36ea4b1b146f72d9332
Vital E <vitalyyemelyanov6@gmail.com>
2023-01-30 11:49:54 +0300

    changes pom.xml

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,8 @@ public class Main {
         BonusService service = new BonusService();
 
         int expected = 30;
-        long actual =service.calculate(1000,true);
+        long actual = service.calculate(1000, true);
+
         System.out.println(expected + "== ? ==" + actual);
     }
 }
